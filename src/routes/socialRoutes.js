bd0ad051f6f0bca579fb7b7c259f072bcdddb128
krishnarajput <161302830+KrishnpalJadam@users.noreply.github.com>
2026-07-28 14:09:07 +0530

    Add delete chat APIs for super admin

--- src/routes/socialRoutes.js
+++ src/routes/socialRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const socialController = require('../controllers/socialController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
+const { authMiddleware, rbacMiddleware } = require('../middlewares/authMiddleware');
 const upload = require('../middlewares/uploadMiddleware');
 
 router.get('/conversations', authMiddleware, socialController.getConversations);
@@ -9,4 +9,9 @@ router.get('/messages/:phone', authMiddleware, socialController.getMessagesByPho
 router.post('/messages/send', authMiddleware, socialController.sendSocialMessage);
 router.post('/upload-media', authMiddleware, upload.single('file'), socialController.uploadMedia);
 router.get('/media-proxy', socialController.proxyTwilioMedia);
+
+// Delete Routes (Super Admin Only)
+router.delete('/messages/:id', authMiddleware, rbacMiddleware(['super_admin']), socialController.deleteMessage);
+router.delete('/conversations/:phone', authMiddleware, rbacMiddleware(['super_admin']), socialController.clearChat);
+
 module.exports = router;
